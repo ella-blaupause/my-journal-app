fa8527a3f732f897c26a4c406d097c6bfdff5f8a
Ella Kempel <ella.lernt@mail.de>
2023-03-11 12:23:52 +0100

    add style

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -4,10 +4,14 @@ import Tab from "../Tab/Tab";
 export default function Tabs() {
   return (
     <div className="tabs">
+      {/* <li> */}
       <Tab color="var(--color-nemo)" countColor="white" count="3">
         All Entries
       </Tab>
+      {/*  </li>
+      <li> */}
       <Tab count="1">Favorites</Tab>
+      {/* ‚ */}
     </div>
   );
 }
